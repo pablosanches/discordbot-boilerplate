6858438a81780e2f7924925d7bbffda75689ce47
Pablo Sanches <sanches.webmaster@gmail.com>
2021-08-02 16:08:03 -0300

    config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 var config = { // NOTE: DO NOT LEAVE ANYTHING BLANK
 	// ALL settings are strings. Do NOT just use true or false, use these in strings such as 'true' or 'false'. This is due to how some code works when changing the settings
         ownerID: '246652516249370624', // Your ID here
-        token: 'ODcwNzE5MDA0Njc3OTI2OTEz.YQQ2Qw.BfLDCns-tG7TlFKv7_6dUiUXA0s', // Your bot token here
+        token: '', // Your bot token here
         status: 'online', // Bot status [online/idle/invisible/dnd]
         debug: 'false', // This is used to output some debug info if needed. The token will be in the console and other information could be in the console
         playingGame: '{{prefix}}help | {{guilds}} guilds | v{{version}}', // The game you want the bot to play. {{prefix}} is replaced with the default prefix below, {{guilds}} is replaced with the guild count and {{version}} is replaced with the bot version. Leave blank to disable
